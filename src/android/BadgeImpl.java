4f59ebc0e4a68b126b96377d0ad392ffe6772379
Denis Visan <denis.visan@inomedica.ro>
2017-06-29 09:12:26 +0300

    Kept only Android implementation

--- src/android/BadgeImpl.java
+++ src/android/BadgeImpl.java
@@ -17,11 +17,11 @@
 
 package de.appplant.cordova.plugin.badge;
 
+import java.lang.Math;
+
 import android.content.Context;
 import android.content.SharedPreferences;
 
-import org.apache.cordova.CallbackContext;
-import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -31,20 +31,17 @@ import me.leolin.shortcutbadger.ShortcutBadger;
 /**
  * Implementation of the badge interface methods.
  */
-class BadgeImpl {
+public class BadgeImpl {
 
     // The name for the shared preferences key
     private static final String BADGE_KEY = "badge";
-
-    // The name for the shared preferences key
-    private static final String CONFIG_KEY = "badge.config";
 
     /**
      * Clear the badge number.
      *
      * @param ctx The application context.
      */
-    void clearBadge (Context ctx) {
+    public void clearBadge (Context ctx) {
         saveBadge(0, ctx);
         ShortcutBadger.removeCount(ctx);
     }
@@ -53,65 +50,26 @@ class BadgeImpl {
      * Get the badge number.
      *
      * @param ctx      The application context.
-     * @param callback The function to be exec as the callback.
+	 *
+	 * @return The current badge count
      */
-    void getBadge (CallbackContext callback, Context ctx) {
+    public int getBadge (Context ctx) {
         SharedPreferences settings = getSharedPreferences(ctx);
         int badge = settings.getInt(BADGE_KEY, 0);
-        PluginResult result;
-
-        result = new PluginResult(PluginResult.Status.OK, badge);
-
-        callback.sendPluginResult(result);
+        
+		return int;
     }
 
     /**
      * Set the badge number.
      *
-     * @param args The number to set as the badge number.
+     * @param badge The number to set as the badge number.
      * @param ctx  The application context
      */
-    void setBadge (JSONArray args, Context ctx) {
-        int badge = args.optInt(0);
-
+    public void setBadge (int badge, Context ctx) {
+		badge = Math.max(badge, 0);
         saveBadge(badge, ctx);
         ShortcutBadger.applyCount(ctx, badge);
-    }
-
-    /**
-     * Get the persisted config map.
-     *
-     * @param ctx      The application context.
-     * @param callback The function to be exec as the callback.
-     */
-    void loadConfig(CallbackContext callback, Context ctx) {
-        SharedPreferences settings = getSharedPreferences(ctx);
-        String json       = settings.getString(CONFIG_KEY, "{}");
-        JSONObject config;
-
-        try {
-            config = new JSONObject(json);
-        } catch (JSONException e) {
-            config = new JSONObject();
-        }
-
-        PluginResult result;
-        result = new PluginResult(PluginResult.Status.OK, config);
-
-        callback.sendPluginResult(result);
-    }
-
-    /**
-     * Persist the config map so that `autoClear` has same value after restart.
-     *
-     * @param config The config map to persist.
-     * @param ctx    The application context.
-     */
-    void saveConfig(JSONObject config, Context ctx) {
-        SharedPreferences.Editor editor = getSharedPreferences(ctx).edit();
-
-        editor.putString(CONFIG_KEY, config.toString());
-        editor.apply();
     }
 
     /**
